d77755c38d0f0dd99cc6486e44a2e67cfda2007a
rhwang123 <rhwang@alteryx.com>
2017-08-24 12:00:23 -0600

    US26843: refactored code

--- PyMultipleInputsToolExample/PyMultipleInputsToolExampleEngine.py
+++ PyMultipleInputsToolExample/PyMultipleInputsToolExampleEngine.py
@@ -2,35 +2,26 @@ import AlteryxPythonSDK as Sdk
 import xml.etree.ElementTree as Et
 
 
-class AyxPlugin:
-    def __init__(self, n_tool_id, alteryx_engine, generic_engine, output_anchor_mgr):
-        self.__n_tool_id = n_tool_id
-        self.name = str('PyMultiInput_') + str(self.__n_tool_id)
-        self.__alteryx_engine = alteryx_engine
-        self.__generic_engine = generic_engine
-        self.__output_anchor_mgr = output_anchor_mgr
-        self.output_anchor = None
-        self.record_info_out = None
-        self.record_creator = None
-        self.is_left = True
+class NonInterface:
+    def __init__(self):
         self.xml_out = None
         self.hash_right = None
         self.hash_left = None
         self.fields_right = []
         self.fields_left = []
-        pass
-    
+
     @property
     def is_compatible(self):
+        # Returns true if same pattern of zeroes and ones in the data sets
         if self.hash_left == self.hash_right:
             return True
         else:
             return False
-            
+
     def is_equal(self, index: int):
         # Returns true if the `other_field` argument has the same type, size, and scale as this field.
         return Sdk.Field.equal_type(self.fields_right[index], self.fields_left[index])
-    
+
     def validate_inputs(self, left_count: int, right_count: int):
         if left_count != right_count:
             raise self.__alteryx_engine.output_message(
@@ -39,16 +30,16 @@ class AyxPlugin:
                 'Both inputs must have the same number of fields.'
             )
         pass
-    
+
     @property
     def __xml_out(self):
         return self.xml_out
-    
+
     @__xml_out.setter
     def __xml_out(self, xml_string: str):
         self.xml_out = xml_string
         pass
-            
+
     @staticmethod
     def change_to_bool(xml_string: str):
         root = Et.fromstring(xml_string)
@@ -57,29 +48,78 @@ class AyxPlugin:
             field.set('size', '1')
         return Et.tostring(root, encoding='utf8', method='xml').decode('utf8')
 
+
+class AyxPlugin(NonInterface):
+    def __init__(self, n_tool_id, alteryx_engine, generic_engine, output_anchor_mgr):
+        """Initializing members that will be used."""
+        super().__init__()
+        self.__n_tool_id = n_tool_id
+        self.name = str('PyMultiInput_') + str(self.__n_tool_id)
+        self.__alteryx_engine = alteryx_engine
+        self.__generic_engine = generic_engine
+        self.__output_anchor_mgr = output_anchor_mgr
+        self.output_anchor = None
+        self.record_info_out = None
+        self.record_creator = None
+        self.is_left = True  # flag for distinguishing input flow
+        pass
+
     def pi_init(self, str_xml):
+        """
+        Called when the Alteryx engine is ready to provide the tool configuration from the GUI.
+        :param str_xml: The raw XML from the GUI
+        :return: Void
+        """
+        # Getting the output anchor from Config.xml by the output connection name
         self.output_anchor = self.__output_anchor_mgr.get_output_anchor('Output')
         pass
 
     def pi_add_incoming_connection(self, str_type, str_name):
+        """
+        Called when the Alteryx engine is attempting to add an incoming data connection.
+        :param str_type: The type of each input connection.
+        :param str_name: A unique name for each input connection.
+        :return: Self, and a reference to an object.
+        """
         return self
 
     def pi_add_outgoing_connection(self, str_name):
+        """
+        Called when the Alteryx engine is attempting to add an outgoing data connection.
+        :param str_name: A unique name for each output connection.
+        :return: Boolean, where True signifies that the connection is accepted.
+        """
         return True
 
     def pi_push_all_records(self, n_record_limit):
-        self.__alteryx_engine.output_message(
-            self.__n_tool_id,
-            Sdk.EngineMessageType.error,
-            'Misssing Incoming Connection'
-        )
+        """
+        Called when the Alteryx engine when it's expecting the plugin to provide all of its data.
+        Only pertinent to tools which have no upstream connections, like the Input tool.
+        :param n_record_limit: Set it to <0 for no limit, 0 for no records, and >0 to specify the number of records.
+        :return: False
+        """
+        self.__alteryx_engine.output_message(self.__n_tool_id, Sdk.EngineMessageType.error, 'Misssing Incoming Connection')
         return False
         
-    def pi_close(self, b_has_errors):        
+    def pi_close(self, b_has_errors):
+        """
+        Called after all data has finished flowing through all the fields.
+        :param b_has_errors: Boolean; set to true to not do the final processing.
+        :return: Void
+        """
         self.output_anchor.assert_close()
         pass
         
     def ii_init(self, record_info_in):
+        """
+        For each connection:
+            1) Handles hash extraction from the data
+            2) Stores the references of each record_info_in field into an array
+            3) Building out the data structure layout for the record_info_out reference
+        Called when the incoming connection's record metadata is available or has changed, and
+        :param record_info_in: A RecordInfo object containing the XML representation for the incoming connection's field and sort properties.
+        :return: True
+        """
         if self.is_left:
             self.hash_left = record_info_in.get_hash()
             
@@ -108,6 +148,11 @@ class AyxPlugin:
         return True
         
     def ii_push_record(self, in_record):
+        """
+        Responsible for pushing out the evaluated boolean results for both the is_equal() and is_compatible() custom tests
+        :param in_record: The data for the incoming record.
+        :return: False
+        """
         if self.is_left:
             return False
         else:
@@ -135,6 +180,11 @@ class AyxPlugin:
             return False
         
     def ii_update_progress(self, d_percent):
+        """
+        Called when the incoming connection is requesting that the plugin update its progress.
+        :param d_percent: Value between 0 and 1
+        :return: Void
+        """
         if self.is_left:
             pass
         else:
@@ -143,6 +193,11 @@ class AyxPlugin:
             pass
 
     def ii_close(self):
+        """
+        Responsible for setting the stream flow to focus on the right input
+        Called when the incoming connection has finished passing all of its records.
+        :return: Void
+        """
         if self.is_left:
             self.is_left = not self.is_left
         else:
