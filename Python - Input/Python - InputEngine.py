459911efcd2a7cf11c39392ee07e058945fde42d
Unknown <odembowski@alteryx.com>
2017-10-12 09:33:23 -0700

    Fixing duplicates

--- Python - Input/Python - InputEngine.py
+++ Python - Input/Python - InputEngine.py
@@ -144,9 +144,6 @@ class AyxPlugin:
         # Make sure that the output anchor is closed.
         self.output_anchor.close()
 
-        # Make sure that the output anchor is closed.
-        self.output_anchor.close()
-
         return True
 
     def pi_close(self, b_has_errors: bool):
